32021d90370d035ff4a639a85c6a8e5ec8758d58
Gourd <Gourd112@proton.me>
2025-08-22 00:02:57 +0000

    Testing Out Codespaces

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -3,11 +3,12 @@
 // Using the Exercise Files in GitHub Codespaces, by Eduardo Corpeño 
 
 #include <iostream>
+using namespace std;
 
 int main(){
     float num_1, num_2, result;
 
-    std::cout << "Enter number 1: " << std::flush;
+    cout << "Enter number 1: " << std::flush;
     std::cin >> num_1;
     std::cout << "Enter number 2: " << std::flush;
     std::cin >> num_2;
